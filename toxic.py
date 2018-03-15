b02cd90fec64cf53a84f9dbfbe53c5b694b8174b
sanketkkeni <sanketkkeni@gmail.com>
2018-03-15 00:25:29 -0700

    changes made to kaggle base code

--- toxic.py
+++ toxic.py
@@ -1,60 +1,71 @@
+# Sanket Keni 
+
 import numpy as np
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
+from playsound import playsound
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+from scipy.sparse import hstack
+import timeit
+
+# notify when code has completed execution
+def audio():
+    playsound('C:\\Users\\Sanket Keni\\Music\\gang.mp3')
 
 
-test = pd.read_csv("C:\\Users\\Sanket Keni\\Desktop\\Genesis\\toxic comment\\test.csv")
-train = pd.read_csv("C:\\Users\\Sanket Keni\\Desktop\\Genesis\\toxic comment\\train.csv")
-sample = pd.read_csv("C:\\Users\\Sanket Keni\\Desktop\\Genesis\\toxic comment\\sample_submission.csv")
+class_names = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
 
+train = pd.read_csv('C:\\Users\\Sanket Keni\\Desktop\\Genesis\\toxic comment\\train.csv').fillna(' ')
+test = pd.read_csv('C:\\Users\\Sanket Keni\\Desktop\\Genesis\\toxic comment\\test.csv').fillna(' ')
 
-def output_one_classifier(test_col):
-    
-    df_x=train["comment_text"]
-    df_y=train[test_col]
-    x_train, x_test, y_train, y_test = train_test_split(df_x, df_y, test_size=0.2, random_state=4)
-
-    cv = TfidfVectorizer(min_df=1,stop_words='english', max_features=1000)
-    x_train_trans=cv.fit_transform(x_train)
-    a=x_train_trans.toarray()
-
-    x_test_trans=cv.transform(x_test)
-    x_test_trans.toarray()
-
-    mnb = MultinomialNB()
-    mnb.fit(x_train_trans,y_train)
-    predictions=mnb.predict(x_test_trans)
-    a=np.array(y_test)
-    count=0
-    for i in range (len(predictions)):
-        if predictions[i]==a[i]:
-            count=count+1
-    accuracy = count*100/len(predictions)
-    print("cross validation accuracy for", test_col,"column is:", accuracy)     
-    df_test=test["comment_text"]    
-    test_inp = cv.transform(df_test)
-    test_inp.toarray()    
-    out = mnb.predict(test_inp)
-    return out
-    
-toxic = output_one_classifier("toxic")  
-severe_toxic = output_one_classifier("severe_toxic")   
-obscene = output_one_classifier("obscene")      
-threat = output_one_classifier("threat")      
-insult = output_one_classifier("insult")     
-identity_hate = output_one_classifier("identity_hate")     
-id = test["id"].values
-    
-out_df = pd.DataFrame({"id": id, "toxic": toxic, "severe_toxic": severe_toxic, "obscene": obscene, "threat": threat, "insult": insult, "identity_hate": identity_hate})   
-    
-out_df.to_csv("C:\\Users\\Sanket Keni\\Desktop\\Genesis\\toxic comment\\out.csv", index=False)    
-    
+train_text = train['comment_text']
+test_text = test['comment_text']
+all_text = pd.concat([train_text, test_text])
 
 
 
+word_vectorizer = TfidfVectorizer(
+    sublinear_tf=True,
+    strip_accents='unicode',
+    analyzer='word',
+    token_pattern=r'\w{1,}',
+    stop_words='english',
+    ngram_range=(1, 1),
+    max_features=5000)
+word_vectorizer.fit(all_text)
+train_word_features = word_vectorizer.transform(train_text)
+test_word_features = word_vectorizer.transform(test_text)
+#audio()
 
+char_vectorizer = TfidfVectorizer(
+    sublinear_tf=True,
+    strip_accents='unicode',
+    analyzer='char',
+    stop_words='english',
+    ngram_range=(2, 3),
+    max_features=5000)
+char_vectorizer.fit(all_text)
+train_char_features = char_vectorizer.transform(train_text)
+test_char_features = char_vectorizer.transform(test_text)
+audio()
 
+train_features = hstack([train_char_features, train_word_features])
+test_features = hstack([test_char_features, test_word_features])
 
+scores = []
+submission = pd.DataFrame.from_dict({'id': test['id']})
+
+for class_name in class_names:
+    train_target = train[class_name]
+    classifier = LogisticRegression(C=0.1, solver='sag')
+    cv_score = np.mean(cross_val_score(classifier, train_features, train_target, cv=3, scoring='roc_auc'))
+    scores.append(cv_score)
+    print('CV score for class {} is {}'.format(class_name, cv_score))
+    classifier.fit(train_features, train_target)
+    submission[class_name] = classifier.predict_proba(test_features)[:, 1]
+
+print('Total CV score is {}'.format(np.mean(scores)))
+
+submission.to_csv('C:\\Users\\Sanket Keni\\Desktop\\Genesis\\toxic comment\\out.csv', index=False)
+audio()
