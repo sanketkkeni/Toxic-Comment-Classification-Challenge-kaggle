464dd296e4343010400d21ea1048681fc89c63e7
Sanket Keni <32827053+sanketkkeni@users.noreply.github.com>
2018-03-15 23:21:16 -0700

    Adjust hyperparameters of models

--- toxic.py
+++ toxic.py
@@ -1,13 +1,14 @@
 # Sanket Keni 
 '''
-CV score for class toxic is 0.9686084660150955
-CV score for class severe_toxic is 0.9871819724671885
-CV score for class obscene is 0.9925029450044938
-CV score for class threat is 0.9806019327200612
-CV score for class insult is 0.9769709557584437
-CV score for class identity_hate is 0.9730129481196328
-Total CV score is 0.9798132033474859
-Public LB - 0.9752
+CV score for class toxic is 0.97531893271232
+CV score for class severe_toxic is 0.9882911811070457
+OBSCENE class
+CV score for class obscene is 0.9918359359724859
+CV score for class threat is 0.9871264302579318
+CV score for class insult is 0.9805883409571478
+CV score for class identity_hate is 0.9810203713351426
+Total CV score is 0.984030198723679
+Public LB - 0.9778
 '''
 import numpy as np
 import pandas as pd
@@ -20,8 +21,19 @@ from sklearn.model_selection import cross_val_score
 from scipy.sparse import hstack
 import timeit
 import re
+############### send notification on smartphone
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+url = 'https://www.pushsafer.com/api' # Set destination URL here
+post_fields = {                       # Set POST fields here
+	"t" : "Python code execution complete",
+	"m" : "task finished",
+	"d" : "a",
+	"u" : url,
+	"k" : "***********"
+	}
 
-stemmer = nltk.stem.snowball.SnowballStemmer('english')
+#stemmer = nltk.stem.snowball.SnowballStemmer('english')
 
 # notify when code has completed execution
 def audio():
@@ -67,17 +79,26 @@ def cleaned(comment):
     comment=re.sub("\d{1,}","",comment)
     comment=re.sub("\.{1,}",".",comment)
     comment=re.sub("\:{1,}","",comment)
-    comment=re.sub("\;"," ",comment)
+    comment=re.sub("\;|\=|\%|\^"," ",comment)
     comment=re.sub("\""," ",comment)
-    comment=re.sub("\'"," ",comment)
-    '''comment=re.sub("\(.*?\)","",comment)
-    comment=re.sub("\"|:|@|,|\/|\=|;|\.|\'|\?|\!|\||\+|\~|\-|\#"," ",comment)
+    comment=re.sub("\'{2,}","",comment)
+    comment=re.sub("\/|\!"," ",comment)
+    comment=re.sub("\?"," ",comment)
+    comment=re.sub("\#"," ",comment)
+    comment=re.sub("\,|\@|\|"," ",comment)
+    comment=re.sub("\(|\)"," ",comment)
+    comment=re.sub("\S+jpg"," ",comment)
+    comment=re.sub("\S*wikip\S+","",comment)               
+    comment=re.sub("\[.*?\]"," ",comment)
+    comment=re.sub("\-"," ",comment)
+    '''comment=re.sub("\"|:|@|,|\/|\=|;|\.|\'|\?|\!|\||\+|\~|\-|\#"," ",comment)
     comment=re.sub("\.{1,}",".",comment)
     comment=re.sub("\[.*?\]","",comment)
     comment=re.sub("www\S+","",comment)
     comment=re.sub("\_"," ",comment)
     comment=re.sub("http","",comment)'''
     comment=re.sub("\s+"," ",comment)
+    comment = ' '.join( [w for w in comment.split() if len(w)>1] )
     comment = comment.strip()
     return comment
 
@@ -85,37 +106,31 @@ def cleaned(comment):
 l = list(train['comment_text'])
 
 l[:50]
-s = train['comment_text'][48]
+s = train['comment_text'][159160]
 re.sub("\/"," ",s)
-d = "mdksc dsdj; == dck"
-re.sub("\=|;","",d)
+
+d = "t kwh m^ peaking. "
+re.sub("\;|\=|\%|\^"," ",d)
 re.sub("http","",s)
 '''
+'''
+s = ' '.join( [w for w in s.split() if len(w)>1] )
+
 cleaned(s)
-df=train["comment_text"].apply(lambda x: len(re.findall("\s+'{2,}",str(x)))+1)
+df=train["comment_text"].apply(lambda x: len(re.findall("\[.*?\]",str(x)))+1)
 
-
-
+'''
 
 train['comment_text']=train['comment_text'].apply(lambda x :cleaned(x))
 test['comment_text']=test['comment_text'].apply(lambda x :cleaned(x))
 
 
-
-
-
-
 class_names = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
-
-
-
 
 
 train_text = train['comment_text']
 test_text = test['comment_text']
 all_text = pd.concat([train_text, test_text])
-
-
 
 word_vectorizer = TfidfVectorizer(
     sublinear_tf=True,
@@ -140,7 +155,7 @@ char_vectorizer = TfidfVectorizer(
 char_vectorizer.fit(all_text)
 train_char_features = char_vectorizer.transform(train_text)
 test_char_features = char_vectorizer.transform(test_text)
-audio()
+notify()
 
 train_features = hstack([train_char_features, train_word_features])
 test_features = hstack([test_char_features, test_word_features])
@@ -149,18 +164,37 @@ scores = []
 submission = pd.DataFrame.from_dict({'id': test['id']})
 
 for class_name in class_names:
-    if (class_name != "obscene"):
+    if (class_name in ['toxic']):
         train_target = train[class_name]
-        classifier = LogisticRegression(C=0.1, solver='sag')
+        classifier = LogisticRegression(C=0.63, solver='sag', class_weight = "balanced") # sag arge datasets and bivariate
         cv_score = np.mean(cross_val_score(classifier, train_features, train_target, cv=3, scoring='roc_auc'))
         scores.append(cv_score)
         print('CV score for class {} is {}'.format(class_name, cv_score))
         classifier.fit(train_features, train_target)
         submission[class_name] = classifier.predict_proba(test_features)[:, 1]
-    else:
+    
+    elif(class_name in ["severe_toxic", "insult"]):
+        train_target = train[class_name]
+        classifier = LogisticRegression(C=0.38, solver='sag') # sag large datasets and bivariate
+        cv_score = np.mean(cross_val_score(classifier, train_features, train_target, cv=3, scoring='roc_auc'))
+        scores.append(cv_score)
+        print('CV score for class {} is {}'.format(class_name, cv_score))
+        classifier.fit(train_features, train_target)
+        submission[class_name] = classifier.predict_proba(test_features)[:, 1]
+        
+    elif(class_name in ["threat", "identity_hate"]):
+        train_target = train[class_name]
+        classifier = LogisticRegression(C=0.45, solver='sag') # sag large datasets and bivariate
+        cv_score = np.mean(cross_val_score(classifier, train_features, train_target, cv=3, scoring='roc_auc'))
+        scores.append(cv_score)
+        print('CV score for class {} is {}'.format(class_name, cv_score))
+        classifier.fit(train_features, train_target)
+        submission[class_name] = classifier.predict_proba(test_features)[:, 1]
+        
+    elif(class_name == "obscene"):
         print("OBSCENE class")
         train_target = train[class_name]
-        classifier = Ridge(alpha=20, solver='auto',max_iter=100, random_state=22,  tol=0.0025)
+        classifier = Ridge(alpha=20, solver='auto',max_iter=100, random_state=22,  tol=0.0005)
         cv_score = np.mean(cross_val_score(classifier, train_features, train_target, cv=3, scoring='roc_auc'))
         scores.append(cv_score)
         print('CV score for class {} is {}'.format(class_name, cv_score))
@@ -168,6 +202,6 @@ for class_name in class_names:
         submission[class_name] = classifier.predict(test_features)
 
 print('Total CV score is {}'.format(np.mean(scores)))
-
+notify()
 submission.to_csv('C:\\Users\\Sanket Keni\\Desktop\\Genesis\\toxic comment\\out.csv', index=False)
 audio()
