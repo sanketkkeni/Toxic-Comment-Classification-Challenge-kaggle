6ea349ac97324cc6e2a1283f9855185e40902de3
Sanket Keni <32827053+sanketkkeni@users.noreply.github.com>
2018-03-15 17:50:56 -0700

    Update toxic.py
    Added Ridge model for obscene class

--- toxic.py
+++ toxic.py
@@ -1,20 +1,19 @@
 # Sanket Keni 
 '''
-CV score for class toxic is 0.9682470069323074
-CV score for class severe_toxic is 0.9872657124996594
-CV score for class obscene is 0.9843951465629428
-CV score for class threat is 0.9810181759466508
-CV score for class insult is 0.9767931386642288
-CV score for class identity_hate is 0.9730722219508444
-Total CV score is 0.9784652337594388
-
-Kaggle - 0.9738
+CV score for class obscene is 0.9924251410593588
+CV score for class threat is 0.9810178204032552
+CV score for class insult is 0.9767931587450045
+CV score for class identity_hate is 0.9730725188729847
+Total CV score is 0.9798035734017961
+Public LB- kaggle - 0.9748
 '''
+
 import numpy as np
 import pandas as pd
 from playsound import playsound
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import Ridge
 from sklearn.model_selection import cross_val_score
 from scipy.sparse import hstack
 import timeit
@@ -32,8 +31,6 @@ test = pd.read_csv('C:\\Users\\Sanket Keni\\Desktop\\Genesis\\toxic comment\\tes
 train_text = train['comment_text']
 test_text = test['comment_text']
 all_text = pd.concat([train_text, test_text])
-
-
 
 word_vectorizer = TfidfVectorizer(
     sublinear_tf=True,
@@ -67,13 +64,23 @@ scores = []
 submission = pd.DataFrame.from_dict({'id': test['id']})
 
 for class_name in class_names:
-    train_target = train[class_name]
-    classifier = LogisticRegression(C=0.1, solver='sag')
-    cv_score = np.mean(cross_val_score(classifier, train_features, train_target, cv=3, scoring='roc_auc'))
-    scores.append(cv_score)
-    print('CV score for class {} is {}'.format(class_name, cv_score))
-    classifier.fit(train_features, train_target)
-    submission[class_name] = classifier.predict_proba(test_features)[:, 1]
+    if (class_name != "obscene"):
+        train_target = train[class_name]
+        classifier = LogisticRegression(C=0.1, solver='sag')
+        cv_score = np.mean(cross_val_score(classifier, train_features, train_target, cv=3, scoring='roc_auc'))
+        scores.append(cv_score)
+        print('CV score for class {} is {}'.format(class_name, cv_score))
+        classifier.fit(train_features, train_target)
+        submission[class_name] = classifier.predict_proba(test_features)[:, 1]
+    else:
+        print("OBSCENE class")
+        train_target = train[class_name]
+        classifier = Ridge(alpha=20, solver='auto',max_iter=100, random_state=22,  tol=0.0025)
+        cv_score = np.mean(cross_val_score(classifier, train_features, train_target, cv=3, scoring='roc_auc'))
+        scores.append(cv_score)
+        print('CV score for class {} is {}'.format(class_name, cv_score))
+        classifier.fit(train_features, train_target)
+        submission[class_name] = classifier.predict(test_features)
 
 print('Total CV score is {}'.format(np.mean(scores)))
 
